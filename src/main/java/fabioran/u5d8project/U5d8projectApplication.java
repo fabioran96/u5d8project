5cd1bde96d6f9c82efcbeb319384fb8f0b6518ea
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-09-11 16:41:55 +0200

    Initial commit

---
+++ src/main/java/fabioran/u5d8project/U5d8projectApplication.java
@@ -0,0 +1,13 @@
+package fabioran.u5d8project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d8projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d8projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fabioran/u5d8project/U5d8projectApplicationTests.java
@@ -0,0 +1,13 @@
+package fabioran.u5d8project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d8projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
